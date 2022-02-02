20ca94a9f36e0ab45fdcfa6cedb25a692e940b82
kvoid <96412362+x-kvoid-x@users.noreply.github.com>
2022-02-02 17:07:08 +0100

    Update magister.py

--- magister.py
+++ magister.py
@@ -2,6 +2,7 @@ if __name__ == "__main__":
     print("this file is a library.\nthis file is not meant to be run.")
     exit(1)
 
+import config
 import itertools
 import platform
 import sys
@@ -13,9 +14,9 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver import Firefox, FirefoxOptions
 
 if platform.system() == "Windows":
-    GECKODRIVER = join(getcwd(), "geckodriver.exe")
+    DRIVER = join(getcwd(), config.BROWSER)
 else:
-    GECKODRIVER = join(getcwd(), "geckodriver")
+    DRIVER = join(getcwd(), config.BROWSER)
 
 
 class DriverNotFoundError(Exception):
@@ -30,9 +31,17 @@ class Cijfer:
         self.cijfer = cijfer
         self.weging = weging
         self.inhalen = inhalen
+
     @property
     def all(self):
-        return [self.vak, self.description, self.cijfer, self.weging, self.date, self.inhalen]
+        return [
+            self.vak,
+            self.description,
+            self.cijfer,
+            self.weging,
+            self.date,
+            self.inhalen,
+        ]
 
 
 def log(type: str, msg: str):
@@ -53,15 +62,15 @@ class Magister:
         self.school = school
         self.logindata = login_data
 
-        if not isfile(GECKODRIVER):
+        if not isfile(DRIVER):
             raise DriverNotFoundError("ERROR: driver needs to be in folder.")
 
-        log("INFO", '[driver path] = "{}"'.format(GECKODRIVER))
+        log("INFO", '[driver path] = "{}"'.format(DRIVER))
         firefox_opts = FirefoxOptions()
         firefox_opts.headless = nobrowser
 
         log("INFO", "starting client...")
-        self.driver = Firefox(options=firefox_opts, executable_path=GECKODRIVER)
+        self.driver = Firefox(options=firefox_opts, executable_path=DRIVER)
         print("\n\033[93mloading login page...", end="\033[92m")
 
     def login(self):
@@ -131,7 +140,7 @@ class Magister:
                 btn.click()
                 log("INFO", "went to agenda page")
                 break
-    
+
     def go_leermiddelen(self):
         while True:
             try:
@@ -142,11 +151,16 @@ class Magister:
                 btn.click()
                 log("INFO", "went to leermiddelen page")
                 break
-    
+
     def leermiddelen(self) -> dict:
         self.go_leermiddelen()
         for second in range(1, 6):
-            print("\033[92mINFO :\033[0m waiting for leermiddelen to load... [{}/5]".format(second), end='\r')
+            print(
+                "\033[92mINFO :\033[0m waiting for leermiddelen to load... [{}/5]".format(
+                    second
+                ),
+                end="\r",
+            )
             sleep(1)
         print()
 
@@ -157,11 +171,13 @@ class Magister:
             return
 
         leermiddelen = [
-            list(y) for x, y in itertools.groupby(result, lambda z: z == "Digitaal") if not x
+            list(y)
+            for x, y in itertools.groupby(result, lambda z: z == "Digitaal")
+            if not x
         ]
         for (i, _j) in enumerate(leermiddelen):
             leermiddelen[i] = [x for x in leermiddelen[i] if x != ""]
-        
+
         leermiddelen_dict = []
 
         # TODO fix links not working in leermiddelen_dict
@@ -171,24 +187,26 @@ class Magister:
                 continue
             leermiddelen_dict.append(
                 {
-                    'vak': item[0],
-                    'titel': item[1],
-                    'url': self.driver.find_elements_by_tag_name("a")(item[1]).get_attribute('href'),
-                    'ean': item[2]
+                    "vak": item[0],
+                    "titel": item[1],
+                    "url": self.driver.find_elements_by_tag_name("a")(
+                        item[1]
+                    ).get_attribute("href"),
+                    "ean": item[2],
                 }
             )
 
         return leermiddelen_dict
 
-    def cijfers(self, float_notation=',') -> list[Cijfer]:
-        '''
+    def cijfers(self, float_notation=",") -> list[Cijfer]:
+        """
         retrieve the list of all the latest grades in the 'cijfers' section on the magister homepage.
 
         arguments:
             - self
             - float_notation (string) -> default ','\n
             if you want to convert your grades into floats (e.g "6.7" -> 6.7) specify float_notation as '.'
-        '''
+        """
         while True:
             try:
                 btn = self.driver.find_element_by_id("menu-cijfers")
@@ -227,8 +245,11 @@ class Magister:
             }
         """
         for i in cijfers_spl:
-            c = Cijfer(i[0], i[1], i[2], i[3].replace(",", "."), i[4], i[3] == "Inh") if float_notation != ',' else \
-                Cijfer(i[0], i[1], i[2], i[3], i[4], i[3] == "Inh")
+            c = (
+                Cijfer(i[0], i[1], i[2], i[3].replace(",", "."), i[4], i[3] == "Inh")
+                if float_notation != ","
+                else Cijfer(i[0], i[1], i[2], i[3], i[4], i[3] == "Inh")
+            )
             cijfers.append(c)
 
         return cijfers
@@ -247,7 +268,9 @@ class Magister:
 
         while True:
             try:
-                btn = self.driver.find_element_by_tag_name("dna-button") # dna-button = 'uitgebreide weergave'
+                btn = self.driver.find_element_by_tag_name(
+                    "dna-button"
+                )  # dna-button = 'uitgebreide weergave'
             except NoSuchElementException:
                 pass
             else:
