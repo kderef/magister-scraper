3a564babff436b4fa852d5a90d844ba21466d00b
kvoid <96412362+x-kvoid-x@users.noreply.github.com>
2022-02-03 09:57:02 +0100

    opera support

--- config.py
+++ config.py
@@ -1,6 +1,14 @@
+from platform import system
+from getpass import getuser
+
+running_windows = system() == "Windows"
+
 class Browsers:
-    firefox = "geckodriver.exe"
-    chrome  = "chromedriver.exe"
+    firefox = "geckodriver.exe" if running_windows else "geckodriver"
+    chrome  = "chromedriver.exe" if running_windows else "chromedriver"
+
+class Locations:
+    operaGX = f"C:\\{getuser()}\\AppData\\Local\\Programs\\opera.exe"
 
 # -------------------------------------------------- #
 # if using chrome or chromium based:
@@ -8,11 +16,12 @@ class Browsers:
 # if using firefox or firefox based:
 #       BROWSER = Browsers.Firefox
 BROWSER = Browsers.firefox
+# specify the location of your browser (this only applies if you're using a chromium-based)
+USING_OPERA = 0 # set this to 1 if you are using opera or operaGX
 
 # school name, example: SCHOOL = "osghengelo"
 SCHOOL = "schoolname"
 
-# magister login info
 LOGIN = (
     "username",
     "password"
